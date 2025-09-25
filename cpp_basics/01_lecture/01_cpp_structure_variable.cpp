e1b357bbe5ae1651281e6afd6c09bf544c4c2a47
jkmjatin <jatinmehtagaya@gmail.com>
2025-09-25 13:21:04 +0530

    Add 1st C++ code

---
+++ cpp_basics/01_lecture/01_cpp_structure_variable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+    std::cout << "This is the structure of a simple program in C++ Programming Lnaguage\n";
+
+    int integer_var; // declaring a variable
+    integer_var = 99; // initializing a variable
+
+    float float_var = 9.18;
+
+    std::cout << integer_var << std::endl << float_var;
+    
+
+    return 0;
+}
--- python_basics/09_error_handling/01_youtube_manager.py
+++ python_basics/09_error_handling/01_youtube_manager.py
@@ -1,40 +1,63 @@
+import json
 
+def load_data():
+    try:
+        with open("YouTube.txt", 'r') as file:
+            test = json.load(file)
+            print(type(test))
+            return test
+    except FileNotFoundError:
+        return []
+    
+def save_data_helper(videos):
+    with open("YouTube.txt", 'w') as file:
+        json.dump(videos, file)
 
 def list_all_videos(videos):
+    for index, video in enumerate(videos, start = 1):
+        print(f"{index}. {video['name']}, Durarion: {'time'}")
+
+def add_videos(videos):
+    video_name = input("Enter video name: ")
+    video_duraiton = input("Enter video duration: ")
+    videos.append({'name': video_name, 'duration': video_duraiton})
+    save_data_helper(videos)
+
+def update_videos(videos):
     pass
 
-def add_video(videos):
+def delete_videos(videos):
     pass
 
-def update_video(videos):
-    pass
 
-def delete_video(videos):
-    pass
+def main():
+    videos = load_data()
 
-def exit_app():
-    exit()
+    while True:
+        print("\n YouTube Manager by @jkmloom\n Choose an option:\n")
+        print("1. List all YouTube videos")
+        print("2. Add YouTube video")
+        print("3. Update a YouTube video detail/s.")
+        print("4. Delete a YouTube video from list")
+        print("5. Exit the app")
 
-videos = []
+        user_choice = input("Enter your choice:\t")
 
-while True:
-    print("\n YouTube Manager | Choose an option:")
-    print("1. List all YouTube video ")
-    print("2. Add a YouTube video")
-    print("3. Update a YouTube video details")
-    print("4. Delete a YouTube video")
-    print("5. Exit the app")
+        print(videos)
 
-    choice = input("Enter your choice:\t")
+        match user_choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_videos(videos)
+            case '3':
+                update_videos(videos)
+            case '4':
+                update_videos(videos)
+            case '5':
+                break
+            case _: # _ is used as the default case from other languages
+                print("Invalid option..!")
 
-    match choice:
-        case '1':
-            list_all_videos(videos)
-        case '2':
-            add_video(videos)
-        case '3':
-            update_video(videos)
-        case '4':
-            delete_video(videos)
-        case '5':
-            exit_app()
+if __name__ == "__main__":
+    main()
