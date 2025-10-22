38f0c2fe7c70d29a84c7d83729ac6dd0312fddc7
jkmjatin <jatinmehtagaya@gmail.com>
2025-10-22 13:31:03 +0530

    Update 11_handling_apis

--- python_basics/11_handling_apis/01_free_api_username.py
+++ python_basics/11_handling_apis/01_free_api_username.py
@@ -1,3 +1,6 @@
+# requests library: Requests is a simple, yet elegant, HTTP library.
+# requests url: https://pypi.org/project/requests/
+
 import requests
 
 def fetch_random_user_freeapi():
@@ -6,4 +9,20 @@ def fetch_random_user_freeapi():
     data = response.json()
 
     if data["success"] and "data" in data:
-        pass
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        user_country = user_data["location"]["country"]
+        return username, user_country
+    else:
+        raise Exception("Failed to fetch user data")
+    
+def main():
+    try:
+        usrename, user_country = fetch_random_user_freeapi()
+        print(f"Username: {usrename}\nCountry: {user_country}")
+    except Exception as e:
+        print(str(e))
+
+# __dunder_method__
+if __name__ == "__main__":
+    main()
