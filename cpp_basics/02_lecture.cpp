d9bd48c7a489e924a55496969a0dda5ba9efdf76
jkmloom <jatinmehtagaya@gmail.com>
2025-10-29 11:39:46 +0530

    Update 01_lecture.cpp

--- cpp_basics/02_lecture.cpp
+++ cpp_basics/02_lecture.cpp
@@ -3,8 +3,36 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+// starting function
+int starter(){
     cout << "Namaste Duniya" << endl;
-    cout << "@jkmloom";
+    cout << "@jkmloom" << endl;
+
+    return 0;
+}
+
+
+// main function
+int main(int argc, char const *argv[])
+{
+    // called the stater function
+    starter();
+
+    int age = 25;
+    float grade = 9.7;
+    bool fat = true; // true = 1, and false = 0
+    char alpha = 'a';
+    
+    string str_value = "Namaste"; // std::string
+    char word[] = "Anime";
+
+    cout << age << endl;
+    cout << grade << endl;
+    cout << fat << endl;
+    cout << alpha << endl;
+
+    cout << str_value << endl;
+    cout << word << endl;
+    
     return 0;
 }
