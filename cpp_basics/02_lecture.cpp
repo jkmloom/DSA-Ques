99d491ecb1fed870913612e214447cc46634b33d
jkmloom <jatinmehtagaya@gmail.com>
2025-10-29 11:24:19 +0530

    Add initial CPP file from lecture 01

---
+++ cpp_basics/02_lecture.cpp
@@ -0,0 +1,10 @@
+// Lecture 1 was all theory hence, in the "cpp_basics" directory the file's naming is starting from '2'
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Namaste Duniya" << endl;
+    cout << "@jkmloom";
+    return 0;
+}
