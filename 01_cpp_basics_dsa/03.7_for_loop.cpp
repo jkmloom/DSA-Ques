3ff35e25da5baa8eada9d07c970deb053ecf5b61
jkmloom <jatinmehtagaya@gmail.com>
2025-11-13 19:10:51 +0530

    Add 03.7 to cpp

--- 01_cpp_basics_dsa/03.7_for_loop.cpp
+++ 01_cpp_basics_dsa/03.7_for_loop.cpp
@@ -3,10 +3,24 @@
 using namespace std;
 
 int main(){
-    for(int i = 1; i <= 10; i++){
-        cout << i << " ";
-    }
-    cout << endl;
+    // for(int i = 1; i <= 10; i++){
+    //     cout << i << " ";
+    // }
+    // cout << endl;
 
+    // -----------------------
+
+    // sum of numbers from 1 to n
+    int sum = 0;
+    int n;
+    
+    cout << "Enter trhe value of n:\t";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+
+    cout << "Sum = " << sum << endl;
     return 0;
 }
